e905eb75b5e616022d3b52cf5dcdd9214085c4b1
zlf <820023459@qq.com>
2020-05-17 22:52:41 +0800

    数据图表功能

---
+++ src/components/report/reports.vue
@@ -0,0 +1,73 @@
+<template>
+<di>
+  <!--面包屑导航区-->
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item :to="{path:'/home'}">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+    <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+  </el-breadcrumb>
+  <el-card>
+    <div id="main" style="width: 800px;height:400px;"></div>
+  </el-card>
+</di>
+</template>
+
+<script>
+import echarts from 'echarts'
+// 对象合并 深拷贝
+import _ from 'lodash'
+export default {
+  name: 'reports',
+  data () {
+    return {
+      // 指定图表的配置项和数据
+      option: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {},
+  // dom 渲染成功后
+  async mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    const {data: res} = await this.$http.get('reports/type/1', {params: {type: 3}})
+    if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+    // 合并
+    const result = _.merge(res.data, this.option)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  created () {}
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import treetable from 'vue-table-with-tree-grid'
 import 'element-ui/lib/theme-chalk/index.css'
 // 全局样式需要导入
 import './assets/css/global.css'
-
 // axios引入
 import axios from 'axios'
 axios.defaults.baseURL = 'http://119.23.53.78:8888/api/private/v1/'
@@ -28,6 +27,19 @@ Vue.prototype.$http = axios
 Vue.use(ElementUI)
 Vue.component('tree-table', treetable)
 Vue.config.productionTip = false
+
+// 注册全局过滤器 名字 函数（需要处理的数据）
+Vue.filter('dataformat', function (originval) {
+  const dt = new Date(originval)
+  // 不足两位补0
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + '' + 1).padStart(2, '0')
+  const d = (dt.getDay() + '').padStart(2, '0')
+  const h = (dt.getHours() + '').padStart(2, '0')
+  const min = (dt.getMinutes() + '').padStart(2, '0')
+  const sec = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${h}:${min}:${sec}`
+})
 new Vue({
   el: '#app',
   router,
